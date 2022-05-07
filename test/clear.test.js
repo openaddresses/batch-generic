14a08f55003e6ba60d2586ab1127de5c4ad538a0
ingalls <nick@ingalls.ca>
2022-05-07 16:21:01 -0600

    Add clear test

---
+++ test/clear.test.js
@@ -0,0 +1,51 @@
+import test from 'tape';
+import { sql, createPool } from 'slonik';
+import { Dog } from './base.js';
+
+import './prep.js';
+
+let pool;
+
+test('Create Table', async (t) => {
+    pool = createPool('postgres://postgres@localhost:5432/batch_generic');
+
+    try {
+        await pool.query(sql`
+            CREATE TABLE dog (
+                id          BIGSERIAL,
+                name        TEXT NOT NULL UNIQUE,
+                species     TEXT NOT NULL DEFAULT 'mutt',
+                loyalty     INTEGER NOT NULL DEFAULT 10,
+                cute        BOOLEAN NOT NULL DEFAULT True,
+                smart       BIGINT NOT NULL DEFAULT 100
+            );
+        `);
+
+        await pool.query(sql`
+            INSERT INTO dog (
+                name
+            ) VALUES (
+                'prairie'
+            );
+        `);
+    } catch (err) {
+        t.error(err);
+    }
+
+    t.end();
+});
+
+test('Dog.clear()', async (t) => {
+    try {
+        const dog = await Dog.clear(pool);
+    } catch (err) {
+        t.error(err);
+    }
+
+    t.end();
+});
+
+test('Cleanup', async (t) => {
+    await pool.end();
+    t.end();
+});
