7aa91e99c579458a5ad653425c890c8f12d68e40
ingalls <nick@ingalls.ca>
2025-06-27 09:10:01 -0600

    Remove Test

--- test/schema.base.ts
+++ test/schema.base.ts
@@ -5,11 +5,13 @@ import {
     json,
     timestamp,
     pgTable,
-    varchar
+    varchar,
+    unique
 } from 'drizzle-orm/pg-core';
 
 export const Profile = pgTable('profile', {
     username: varchar().primaryKey(),
+    email: varchar(),
     meta: json().$type<{
         test?: boolean
     }>().notNull().default({}),
@@ -17,4 +19,6 @@ export const Profile = pgTable('profile', {
     updated: timestamp({ withTimezone: true }).notNull().default(sql`Now()`),
     callsign: varchar().notNull().default('Unknown Callsign'),
     location: geometry({ srid: 4326, type: GeometryType.Point })
+}, (t) => {
+    unq: unique().on(t.email)
 });
